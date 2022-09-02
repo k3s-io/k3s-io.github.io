238eac6f9613caa8dde7eb606fc80a3c0c6a4ff8
Derek Nola <derek.nola@suse.com>
2022-09-02 09:45:04 -0700

    Initial commit of landing page
    Signed-off-by: Derek Nola <derek.nola@suse.com>

---
+++ README.md
@@ -0,0 +1,36 @@
+# K3s Website and Docs
+
+This repo contains the content of the K3s landing page. Please open an issue if you have suggestions for new content or edits. We also gladly accept community PRs. 
+
+The website is built using [Docusaurus 2](https://docusaurus.io/), a modern static website generator.
+
+### Installation
+
+```
+$ yarn
+```
+
+### Local Development
+
+```
+$ yarn start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+You can also use Docker to launch the website without needing to install and configure yarn:
+```
+docker run --rm -it -v $PWD:$PWD -w $PWD -p 3000:3000 node yarn start -h 0.0.0.0
+```
+
+### Build
+
+```
+$ yarn build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Deployment
+
+An automated GH action will deploy the website to GitHub Pages once a PR has been merged to `main`.
---
+++ docusaurus.config.js
@@ -0,0 +1,66 @@
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'K3s',
+  tagline: '',
+  url: 'https://k3s-io.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'k3s-io', // Usually your GitHub org/user name.
+  projectName: 'k3s-io.github.io', // Usually your repo name.
+  trailingSlash: false,
+  themeConfig: {
+    colorMode: {
+      // "light" | "dark"
+      defaultMode: "light",
+
+      // Hides the switch in the navbar
+      // Useful if you want to support a single color mode
+      disableSwitch: false,
+    },
+    navbar: {
+      title: "",
+      logo: {
+        alt: 'logo',
+        src: 'img/k3s-logo-light.svg',
+        srcDark: 'img/k3s-logo-dark.svg',
+      },
+      items: [
+        {
+          to:  'https://k3s-io.github.io/docs',
+          position: 'right',
+          label: 'Docs',
+          className: 'navbar__docs',
+        },
+        {
+          to: 'https://github.com/k3s-io/k3s/',
+          label: 'GitHub',
+          position: 'right',
+          className: 'navbar__github btn btn-secondary icon-github',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} K3s Project Authors. All rights reserved. <br>The Linux Foundation has registered trademarks
+      and uses trademarks. For a list of trademarks of The Linux Foundation, 
+      please see our <a href="https://www.linuxfoundation.org/trademark-usage"> Trademark Usage</a> page.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: false,
+        blog: false, // Optional: disable the blog plugin
+        // ...
+        theme: {
+          customCss: [require.resolve("./src/css/custom.css")],
+        },
+      },
+    ],
+  ], 
+};
